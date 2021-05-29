29b78b53a2802b26c049c50d260f979c6f17fded
hz378904298 <378904298@qq.com>
2021-05-29 19:46:33 +0800

    Create jd59.js

---
+++ jd59.js
@@ -0,0 +1,21 @@
+/*
+QX1.0: 
+https:\/\/api\.jd\.m\.com\/client/.action/?/functionld/=/receiveNecklaceCoupon url script-response-body https://raw.githubusercontent.com/hz378904298/Quantumult-X/main/jd59.js
+MITM = api.m.jd.com
+*/
+
+let obj = JSON.parse($response.body);
+obj = {
+  "code": "0",
+ "success": true,
+ "busiCode": "0",
+ "message": "成功",
+ "result": {
+  "necklaceCheckRes": {
+   "resultCode": 2,
+   "necklaceNum": 20000
+  }
+ },
+ "lastFlag": null
+};
+$done({body: JSON.stringify(obj)});
