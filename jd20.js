6a5d4f77ff9ff50a2440ee3024229467ea58ba17
hz378904298 <378904298@qq.com>
2021-05-29 19:43:57 +0800

    Update jd20.js

--- jd20.js
+++ jd20.js
@@ -1,6 +1,6 @@
 /*
 QX:
-https:\/\/api\.jd\.m\.com\/client/.action/?/functionld/=/receiveNecklaceCoupon url script-response-body 
+https:\/\/api\.jd\.m\.com\/client/.action/?/functionld/=/receiveNecklaceCoupon url script-response-body https://raw.githubusercontent.com/hz378904298/Quantumult-X/main/jd20.js
 MITM = api.m.jd.com
 */
 
